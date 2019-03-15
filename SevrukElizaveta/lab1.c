2dee9613fab21efa21168d61b6f949726bca3ff1
SevrukElizaveta <48241246+SevrukElizaveta@users.noreply.github.com>
2019-03-15 07:51:09 +0300

    lab_1

---
+++ SevrukElizaveta/lab1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int findMaximumPowerOfTwoInRange(int rangeStart, int rangeEnd) {
+    int maxPowOfTwoInRange = 1;
+    while (maxPowOfTwoInRange * 2 <= rangeEnd)
+        maxPowOfTwoInRange *= 2;
+    return maxPowOfTwoInRange;
+}
+
+void printBinaryFormOfNumber(int number)
+{
+    if (number > 1)
+        printBinaryFormOfNumber(number / 2);
+    printf("%d", number % 2);
+}
+
+int main(int argc, const char * argv[]) {
+    int rangeStart, rangeEnd;
+    printf("Enter range start: ");
+    scanf("%d", &rangeStart);
+    printf("Enter range end: ");
+    scanf("%d", &rangeEnd);
+    int numWithMaxNumberOfOnes = rangeEnd <= 3 ? rangeEnd : findMaximumPowerOfTwoInRange(rangeStart, rangeEnd) - 1;
+    printf("Number with maximum count of ones in decimal representation: %d\n", numWithMaxNumberOfOnes);
+    printf("Number with maximum count of ones in binary representation: ");
+    printBinaryFormOfNumber(numWithMaxNumberOfOnes);
+    printf("\n");
+    return 0;
+}
